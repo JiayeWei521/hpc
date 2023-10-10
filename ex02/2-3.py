66335898031cb90829085543762fdcb4a5ab03f3
Jiayew521endfalsegit config --global user.name Your <jiaye.wei@epfl.ch>
2023-10-10 09:06:02 +0200

    Add lecture slides

--- ex02/2-3.py
+++ ex02/2-3.py
@@ -1,4 +1,16 @@
-from mpi4py import MPI
+"""
+Last update on 08-10-2023
+
+2-3.py 
+Collective communication - scattering and broadcasting
+
+@author: Jiaye Wei <jiaye.wei@epfl.ch>
+
+To execute the code, do (4 can be replaced by any number of processors):
+mpiexec -n 4 python 2-3.py
+"""
+
+from mpi4py import MPI 
 import numpy as np
 
 # Initialize MPI
@@ -13,6 +25,6 @@ else:
     vector = None
 
 data1 = comm.bcast(vector, root=0)
-data2 = comm.scatter(vector, root=0) # type: ignore
+data2 = comm.scatter(vector, root=0)  # type: ignore
 
 print("rank: ", rank, "data1: ", data1, "data2: ", data2)
--- ex02/2-4.py
+++ ex02/2-4.py
@@ -1,13 +1,77 @@
-from mpi4py import MPI
-import numpy as np
+"""
+Last update on 08-10-2023
 
-# Initialize MPI
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-size = comm.Get_size()
+2-4.py 
+Collective communication - all-to-all and reduce
 
-senddata = rank * np.ones(size, dtype=int)
+@author: Jiaye Wei <jiaye.wei@epfl.ch>
 
-recvdata = comm.alltoall(senddata) # type: ignore
+To execute the code, do (4 can be replaced by any number of processors):
+mpiexec -n 4 python 2-3.py
+"""
 
-print("Process: ", rank, "sending: ", senddata, "receiving: ", recvdata)
+""" def alltoall():
+    from mpi4py import MPI
+    import numpy as np
+
+    # Initialize MPI
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    senddata = rank * np.ones(size, dtype=int)
+
+    recvdata = comm.alltoall(senddata) # type: ignore
+
+    print("Process: ", rank, "sending: ", senddata, "receiving: ", recvdata)
+ """
+
+""" def reduction():
+    from mpi4py import MPI
+    import numpy as np
+
+    # Initialize MPI
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    senddata = rank * np.ones(size, dtype=int)
+    
+    global_result1 = comm.reduce(senddata, op=MPI.SUM, root=0)
+    global_result2 = comm.reduce(rank, op=MPI.MAX, root=0)
+
+    # Print 
+    print("process ", rank, "sending ", senddata)
+
+    # Print the result on the root process
+    if rank == 0:
+        print("Reduction operation 1: ", global_result1, 
+              "\n Reduction operation 2: ", global_result2)
+
+reduction() """
+
+
+def all_reduction():
+    from mpi4py import MPI
+    import numpy as np
+
+    # Initialize MPI
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    senddata = rank * np.ones(size, dtype=int)
+    
+    global_result1 = comm.allreduce(senddata, op=MPI.SUM)
+    global_result2 = comm.allreduce(rank, op=MPI.MAX)
+
+    # Print 
+    print("process ", rank, "sending ", senddata)
+
+    # Print the result on the root process
+    if rank == 0:
+        print("All reduction operation 1: ", global_result1, 
+              "\n",
+              "All reduction operation 2: ", global_result2)
+        
+all_reduction()
